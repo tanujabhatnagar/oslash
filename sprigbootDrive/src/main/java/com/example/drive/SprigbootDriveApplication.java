af554ef57c94a1f989e4963db612207301941214
Tanuja Bhatnagar <44516203+tanuja14@users.noreply.github.com>
2022-09-24 09:29:12 +0530

    added drive API
    added springboot project

---
+++ sprigbootDrive/src/main/java/com/example/drive/SprigbootDriveApplication.java
@@ -0,0 +1,13 @@
+package com.example.drive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprigbootDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprigbootDriveApplication.class, args);
+	}
+
+}
---
+++ sprigbootDrive/src/test/java/com/example/drive/SprigbootDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprigbootDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
